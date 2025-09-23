f2125e2a6650055360e3058e86fd316684e6e473
James Read <contact@jread.com>
2025-09-23 23:26:08 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,18 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
   plugins: [vue()],
   build: {
     rollupOptions: {
       plugins: [
-        visualizer({
-          open: true, // opens the report in your browser
-          filename: 'stats.html',
-          gzipSize: true,
-          brotliSize: true,
-        }),
       ],
     },
   },
